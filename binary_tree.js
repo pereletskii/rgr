051021205080731508acce8d8ce41838ed2ad9ca
Gleb Gorelov <qgorelov@gmail.com>
2024-04-22 22:50:05 +0300

    Binary tree class added

--- binary_tree.js
+++ binary_tree.js
@@ -18,8 +18,48 @@ class Node {
     }
 }
 
-let root = new Node(5);
-root.left = new Node(2);
-root.right = new Node(4);
+class BinaryTree {
+    #root = null;
+    get root() {
+        return this.#root;
+    }
 
-console.log(root, root.left, root.right);
+    insert(value) {
+        let node = new Node(value);
+        if (!this.#root) {
+            this.#root = node;
+        } else {
+            this.#insertNode(this.#root, node);
+        }
+    }
+
+    #insertNode(root, node) {
+        if (node.data < root.data) {
+            if (!root.left) {
+                root.left = node;
+            } else {
+                this.#insertNode(root.left, node);
+            }
+        } else if (node.data > root.data) {
+            if (!root.right) {
+                root.right = node;
+            } else {
+                this.#insertNode(root.right, node);
+            }
+        }
+    }
+
+    getTree() {
+        function printNode(node, count) {
+            console.log(`${'  '.repeat(count)}${node.data}`);
+            if (node.right) printNode(node.right, count + 1);
+            if (node.left) printNode(node.left, count + 1);
+        }
+
+        if (this.#root) {
+            printNode(this.#root, 0);
+        } else return null
+    }
+}
+
+module.exports.BinaryTree = BinaryTree;
