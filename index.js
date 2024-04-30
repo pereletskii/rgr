6e51bf022e19628554666ec7a3f2675613723e76
Gleb Gorelov <qgorelov@gmail.com>
2024-04-30 13:10:31 +0300

    First input architecture created

--- index.js
+++ index.js
@@ -1,27 +1,105 @@
 const fuzz = require('./scripts/fuzzy_number_creation.js');
 const deffas = require('./scripts/deffasification.js');
 const bt = require('./scripts/binary_tree.js');
+const prompt = require("prompt-sync")({ sigint: true });
 
-function random (min, max) {
-    return Math.round(Math.random() * (max - min) + min)
+let exit = false;
+while (!exit) {
+    console.log(
+        "Выберите режим:\n",
+        "   1: Ввод нечётких чисел\n",
+        "   2: Чтение нечётких чисел из JSON\n",
+        "   3: Чтение дерева из JSON\n",
+        "   q: Выход\n"
+    )
+    switch (prompt("Выберите нужный режим: ")) {
+        case "1":
+            fuzzyInput();
+            break;
+        case "2":
+            readTree();
+            break;
+        case "q":
+            exit = true;
+            break;
+        default:
+            console.log("\nТакого варианта нет, повторите ввод\n");
+            break;
+    }
 }
-function randParams(min, max) {
-    let a = random(min, max);
-    let b = random(min, max);
+console.log('\nВыход');
 
-    while (b - a >= b + a){
-        a = random(min, max);
+function isNumber(str) {
+    return !isNaN(parseFloat(str)) && isFinite(str);
+}
+
+function fuzzyInput() {
+    let exit = false;
+    let numsCount = 0;
+
+    let params = {
+        a: [],
+        b: []
     }
 
-    return { a: a, b: b }
-}
-const tree = new bt.BinaryTree();
+    while (!exit){
+        if (numsCount == 0) {
+            console.log(
+                "\nТекущий режим - ввод нечётких чисел. Прекратить ввод - 'q'\n",
+                "Основное условие ввода a > 0\n"
+            );
+        } else {
+            console.log(
+                "\nРанее введённые числа:"
+            );
+            console.table(params);
+            console.log("Прекратить ввод - 'q'\n");
+        }
 
-for (let i = 0; i < 100; i++) {
-    let params = randParams(-50, 50);
-    // let params = { a: 6, b: 9 }
-    let fuzzNum = fuzz.mu(params.a, params.b);
-    let num = deffas.gravityCenter(fuzzNum);
-    tree.insert(num);
+        let a, b;
+        while (true) {
+            a = prompt("a: ");
+            if (a == 'q') {
+                exit = true;
+                break;
+            } else if (isNumber(a)) {
+                a = parseInt(a);
+                if (a <= 0) {
+                    console.log("\nЧисло должно быть положительным\n");
+                    continue;
+                }
+                console.log(a);
+                break;
+            } else {
+                console.log("\nНекорректное значение a\n");
+                continue;
+            }
+        }
+        console.log();
+
+        if (exit) break;
+
+        while (true) {
+            b = prompt("b: ");
+            if (b == 'q') {
+                exit = true;
+                break;
+            } else if (isNumber(b)) {
+                b = parseInt(b);
+                break;
+            } else {
+                console.log("\nНекорректное значение b\n");
+                continue;
+            }
+        }
+        console.log();
+
+        numsCount++;
+        params.a.push(a);
+        params.b.push(b);
+    }
+
+    if (params) {
+        // TODO добавить дефазификацию и дальнейшую работу с деревом
+    }
 }
-tree.getTree();
